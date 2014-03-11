b12677142bd2eb855193b4bfc83b5d25da36a07d
Tyrel Souza <tyrel@tyrelsouza.com>
2014-03-11 10:27:53 -0400

    Added Fabric File with examples Trying to make project independant looking for manage.py automatically local settings modifications update to check if scripts exist to run Adding branch prompting parse git app from remote as well added requirements syning media change production to false added more heroku tasks

--- README.md
+++ README.md
@@ -1,4 +1,50 @@
-fabric_scripts
+Propel Toolbox
 ==============
+Collection of misc. scripts that can help Propel be better.
 
-Collection of fabric scripts
+## Fabric Scripts
+
+### In the project's root there needs to be a file called 'projectconf.py' with the following parameters:
+* ENVIRONMENT_VARIABLES A list of Environment variables to configure
+* DJANGO_PROJECT The name of the django project module
+
+Example:
+
+```python
+    DJANGO_PROJECT = 'fabtastic'
+    ENVIRONMENT_VARIABLES = ['DEBUG','PRODUCTION','DATABASE_NAME']
+```
+
+The reasoning for this to be hardcoded is that it's a catch 22 to read from the
+django settings, you need to know the project name to access settings, so you can't store it in the django settings. (If anyone knows a better way to do this, please create a pull request!)
+
+#### Utility scripts:
+* $ fab help
+    * Displays some help to get you started.
+
+#### Django Setup:
+* $ fab setup
+    * This runs the setup scripts to get the django app working on your server.
+* $ fab development
+    * This runs the development server.
+* $ fab staging
+    * This runs the staging server.
+* $ fab production 
+    * This runs the production server.
+* $ fab test
+    * Runs the default tests. 
+* $ copy_media
+    * Copies local media to s3
+
+
+#### Heroku Setup:
+* $ fab deploy
+    * This prompts for the heroku remote app you want to use, then it turns
+      maintainence on for that branch,
+* $ fab copy_database
+    * This will prompt you where the X.dump file is located, then restore that
+      to the database.
+* $ fab setup_heroku_remotes
+    * Setup the heroku remotes in the format "propel-PROJECT-production" and
+      "propel-PROJECT-staging" and prompting you for the project name. If the
+naming structure differs, please do this manually. 
---
+++ fabfile/django.py
@@ -0,0 +1,132 @@
+import sys, os
+
+from fabric.api import local, lcd
+from fabric.context_managers import shell_env
+
+from projectconf import DJANGO_PROJECT, ENVIRONMENT_VARIABLES
+
+def test():
+    """Django: Runs the default tests"""
+    ENV = _get_env_from_file()
+    ENV['DEBUG'] = 'True'
+    ENV['PRODUCTION'] = ''
+    ENV['STAGING'] = 'True'
+    with shell_env(**ENV):
+        with lcd(_get_run_directory()):
+            local('python manage.py test')
+
+def copy_media():
+    """Django: Copies local media to s3"""
+    ENV = _get_env_from_file()
+    ENV['DEBUG'] = 'True'
+    ENV['PRODUCTION'] = ''
+    ENV['STAGING'] = 'True'
+    with shell_env(**ENV):
+        with lcd(_get_run_directory()):
+            local('python manage.py sync_media_s3')
+
+def development():
+    """Django: Run Server in Dev Mode"""
+    ENV = _get_env_from_file()
+    ENV['DEBUG'] = 'True'
+    ENV['PRODUCTION'] = 'False'
+    ENV['STAGING'] = 'True'
+    with shell_env(**ENV):
+        with lcd(_get_run_directory()):
+            local('python manage.py runserver')
+
+def staging():
+    """Django: Run server in Staging Mode"""
+    ENV = _get_env_from_file()
+    ENV['DEBUG'] = 'False'
+    ENV['PRODUCTION'] = 'False'
+    ENV['STAGING'] = 'True'
+    with shell_env(**ENV):
+        with lcd(_get_run_directory()):
+            local('python manage.py runserver')
+
+def production():
+    """Django: Run server in production mode """
+    ENV = _get_env_from_file()
+    ENV['DEBUG'] = 'False'
+    ENV['PRODUCTION'] = 'True'
+    ENV['STAGING'] = 'False'
+    with shell_env(**ENV):
+        with lcd(_get_run_directory()):
+            local('python manage.py runserver')
+
+def setup():
+    """Django: Setup Environment Variables, then pip install, then syncdb, finally migrate """
+    ENV = _do_env_setup()
+    cwd = _get_run_directory()
+
+    # Run bash scripts
+    if 'scripts' in os.listdir('.') and "setup.sh" in os.listdir('scripts'):
+        local('./scripts/setup.sh')
+    if 'scripts' in os.listdir('.') and "setup_dev.sh" in os.listdir('scripts'):
+        local('./scripts/setup_dev.sh')
+
+    local('pip install -r requirements.txt --allow-all-external')
+
+    with shell_env(**ENV):
+        with lcd(cwd):
+            _local_settings()
+            local('python manage.py syncdb --noinput')
+            local('python manage.py migrate')
+
+
+# Private, not picked up by Fabric
+def _get_run_directory():
+    if not 'manage.py' in os.listdir('.'):
+        cwd = DJANGO_PROJECT
+    else:
+        cwd = '.'
+    return cwd
+def _local_settings():
+    """
+        Copies local_settings.py.default to local_settings.py if needed
+        WILL NOT OVERRIDE
+    """
+    files = os.listdir('.')
+    if "local_settings.py.default" in files and  "local_settings.py" not in files:
+        local('cp local_settings.py.default local_settings.py')
+
+def _do_env_setup():
+    """ Prompt to see if setup environment, if yes, setup, otherwise get env """
+    do_env = raw_input('Setup Environment? (Y/N) ').rstrip("\n")
+    if do_env.lower() == "y":
+        if not ENVIRONMENT_VARIABLES:
+            print "No list 'ENVIRONMENT_VARIABLES' in projectconf.py"
+            return
+        ENV = _get_env_from_input()
+        _write_env_to_file(ENV)
+    else:
+        ENV = _get_env_from_file()
+    return ENV
+
+def _get_env_from_file():
+    """Read KEY=VALUE in from the .env file"""
+    ENV = {}
+    with open(".env", "r") as f:
+        for line in f:
+            key, value = line.rstrip("\n").split("=")
+            ENV[key] = value
+    return ENV
+
+def _write_env_to_file(ENV):
+    """ Write the variables to the .env file """
+    with open(".env", "w") as f:
+        for k,v in ENV.iteritems():
+            print >>f, "{0}={1}".format(k,v)
+
+def _get_env_from_input():
+    """ Propmt the user for new environment variables and returns a dictionary """
+    ENV = {}
+    ENV['DEBUG'] = 'true' # initial in case server is run by foreman
+    ENV['PRODUCTION'] = '' # initial in case server is run by foreman
+    ENV['STAGING'] = 'true' # initial in case server is run by foreman
+    for variable in ENVIRONMENT_VARIABLES:
+        if variable not in ENV:
+            ENV[variable] = raw_input("{0}: ".format(variable.replace("_", " ").title())).rstrip("\n")
+    return ENV
+
