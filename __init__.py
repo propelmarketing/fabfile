c57b14d6cc13609ad013f8de2a2646900dd94374
Tyrel Souza <tyrel@tyrelsouza.com>
2014-03-17 11:15:03 -0400

    Add imports manually into __init__

--- __init__.py
+++ __init__.py
@@ -1,3 +1,3 @@
-from .core import *
-from .django import *
-from .heroku import *
+from .core import update
+from .django import test, copy_media, development, staging, production, setup
+from .heroku import logs, config, config_push, config_pull, collect_static, deploy, copy_database, setup_heroku_remotes, setup_plugins, shell, validate
